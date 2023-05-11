fd94f708431d5694a22915fe59b10ac3a534dd85
davidlopezsotelo <davidaquarium@yahoo.es>
2023-05-11 14:37:18 +0200

    lecion 1

---
+++ app/src/main/java/com/davidlopez/dbsqlite/SQLite.kt
@@ -0,0 +1,20 @@
+package com.davidlopez.dbsqlite
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SQLite(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE  productos ( codigo int primary key,descripcion text,precio real)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
